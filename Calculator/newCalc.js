3aa9a1075bde930f10850924dba50be1bca3d857
Yash James <jamesyash007@gmail.com>
2023-01-10 17:22:27 +0530

    Comments also Added

--- Calculator/newCalc.js
+++ Calculator/newCalc.js
@@ -1,22 +1,30 @@
+// Creating a class for calculator :
 class Calculator {
+    // Consturctor is going to take previous & current operand text ELement:
+    // ---> for taking all the input as well as all the function for the Calculator:
      constructor(previousOperandTextElement,currentOperandTextElement){
         this.previousOperandTextElement = previousOperandTextElement;
         this.currentOperandTextElement = currentOperandTextElement;
         this.clear()
     } //--
 
+    // Now we Will create a function for our Calculator:
+
     clear() {
+        // To clear all the number & operation From Current as well as from previous operand in the whole output area ;
         this.currentOperand = ''
         this.previousOperand = ''
         this.operation = undefined
     }
 
     delete() {
+        // TO Delete a number only single number from current operand ;
         //-- Now delete Button Works 100% Successfull :
         this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     chooseOperation(operation) {
+        // To any time use an operation in the Calculator : ---> and inside this it take a operation "parameter"
         if (this.currentOperand === '') return
         if (this.previousOperand !== '') {
             this.compute()
@@ -28,6 +36,7 @@ class Calculator {
     }
 
     compute() {
+        // To compute a single value that we need to display in the output area ;
         let computation
         const prev = parseFloat(this.previousOperand)
         const current = parseFloat(this.currentOperand)
@@ -77,6 +86,7 @@ class Calculator {
     }
     
     updateDisplay() {
+        // TO update the alculator display after a perticular Calculation
       this.currentOperandTextElement.innerText =
         this.getDisplayNumber(this.currentOperand)
       if (this.operation != null) {
@@ -98,11 +108,18 @@ class Calculator {
  const currentOperandTextElement = document.querySelector('[data-current-operand]');
  // End here.
 
+ // Create a new Calculator Const Variable with variable type object:
  const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement) 
-
+  // Use of thhis const calculator :
+  //--- Use of forEach to Loop over all difernt buttons :
  numberButtons.forEach(button => {
+    // --> for Each of this button we want to add an Event Listener :
+    // -----> And in this Event Listener We want when ever we cliack on this button we want to do something:
     button.addEventListener("click", () => {
+        // What we wanna do we actually want to append this number on output :
+        // ---> What ever inside that button we we erased;
         calculator.appendNumber(button.innerText)
+        // After This we want to update our display in a Calculator :
         calculator.updateDisplay()
     })
  })
